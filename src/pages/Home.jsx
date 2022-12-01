c7c85d65e69a337b4a42f0b9e2a52166cf6b3a36
Azamat <azamatyudaev@gmail.com>
2022-12-02 02:15:58 +0500

    Lesson 5 useState hook

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,35 +1,23 @@
-const Home = () => {
-  const handleClick = (event) => {
-    console.log('hello, ninjas', event)
-  }
+import {useState} from 'react'
 
-  const handleClickAgain = (name, event) => {
-    console.log('hello, ' + name, event)
+const Home = () => {
+  // let name = 'Azamat' // not reactive
+
+  const [name, setName] = useState('Azamat') // reactive
+  const [age, setAge] = useState(27) // reactive
+
+  const handleClick = () => {
+    // name = 'Yudaev'
+    // console.log(name)
+    setName('Yudaev')
+    setAge(28)
   }
 
   return (
     <div className="home">
       <h2>Home Page</h2>
-      {/* Click event runs automatically: handleClick() */}
-      {/*<button type="button" onClick={handleClick()}>Click me</button>*/}
-
-      {/* Click event runs only we click the button: handleClick, Browser event sends automatically */}
+      <p>{name} is {age} years old</p>
       <button type="button" onClick={handleClick}>Click me</button>
-
-      {/* Sending argument and event to function parameter */}
-      <button type="button" onClick={(event) => handleClickAgain('Azamat', event)}>Click me again</button>
-
-      {/*
-        <button type="button" onClick={() => {
-          return handleClickAgain('Azamat')
-        }}>Click me again</button>
-      */}
-
-      {/*
-        <button type="button" onClick={() => {
-          handleClickAgain('Azamat')
-        }}>Click me again</button>
-      */}
     </div>
   )
 }
