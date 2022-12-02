c3bda7720d1f4dac9ec46710297526108380ff60
Azamat <azamatyudaev@gmail.com>
2022-12-02 13:28:18 +0500

    Lesson 8 Reusing components

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -3,14 +3,16 @@ import BlogList from '../components/BlogList'
 
 const Home = () => {
   const [blogs, setBlogs] = useState([
-    { title: 'My new website', body: 'lorem ipsum...', author: 'mario', id: 1 },
+    { title: 'My new website', body: 'lorem ipsum...', author: 'azamat', id: 1 },
     { title: 'Welcome party!', body: 'lorem ipsum...', author: 'yoshi', id: 2 },
-    { title: 'Web dev top tips', body: 'lorem ipsum...', author: 'mario', id: 3 }
+    { title: 'Backend top tips', body: 'lorem ipsum...', author: 'mario', id: 3 },
+    { title: 'Frontend top tips', body: 'lorem ipsum...', author: 'azamat', id: 4 }
   ])
 
   return (
     <div className="home">
       <BlogList blogs={blogs} title="All Blogs!" />
+      <BlogList blogs={blogs.filter((blog) => blog.author === 'azamat')} title="Azamat's Blogs!" />
     </div>
   )
 }
