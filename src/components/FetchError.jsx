30a0ed2395f0e81041e4dd9d6594e1b336f9c96b
Azamat <azamatyudaev@gmail.com>
2022-12-02 22:55:23 +0500

    Lesson 14 Handling fetch errors

---
+++ src/components/FetchError.jsx
@@ -0,0 +1,9 @@
+const Error = ({err}) => {
+  return (
+    <div>
+      {err}
+    </div>
+  )
+}
+
+export default Error
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,21 +1,33 @@
 import { useState, useEffect } from 'react'
 import BlogList from '../components/BlogList'
 import Loader from '../components/Loader'
+import FetchError from '../components/FetchError'
 
 const Home = () => {
   const [blogs, setBlogs] = useState(null)
   const [loading, setLoading] = useState(true)
+  const [error, setError] = useState(null)
 
   // npx json-server --watch data/db.json --port 8000
   useEffect(() => {
     setTimeout(() => {
       fetch('http://localhost:8000/blogs')
         .then((response) => {
+          console.log(response)
+          if (!response.ok) {
+            throw Error('Could not fetch the data for that resource')
+          }
           return response.json()
         })
         .then((data) => {
           console.log(data)
           setBlogs(data)
+          setLoading(false)
+          setError(null)
+        })
+        .catch((err) => {
+          console.log(err.message)
+          setError(err.message)
           setLoading(false)
         })
     }, 1000)
@@ -23,6 +35,7 @@ const Home = () => {
 
   return (
     <div className="home">
+      {error && <FetchError err={error} />}
       {loading && <Loader />}
       {blogs && <BlogList blogs={blogs} title="All Blogs!" />}
     </div>
