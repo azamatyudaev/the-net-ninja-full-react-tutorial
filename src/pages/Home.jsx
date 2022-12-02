292eea6bc9d953944e0efee22ce8e14d205dabb5
Azamat <azamatyudaev@gmail.com>
2022-12-02 22:33:52 +0500

    Lesson 13 Conditional loading message

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,23 +1,29 @@
 import { useState, useEffect } from 'react'
 import BlogList from '../components/BlogList'
+import Loader from '../components/Loader'
 
 const Home = () => {
   const [blogs, setBlogs] = useState(null)
+  const [loading, setLoading] = useState(true)
 
   // npx json-server --watch data/db.json --port 8000
   useEffect(() => {
-    fetch('http://localhost:8000/blogs')
-      .then((response) => {
-        return response.json()
-      })
-      .then((data) => {
-        console.log(data)
-        setBlogs(data)
-      })
+    setTimeout(() => {
+      fetch('http://localhost:8000/blogs')
+        .then((response) => {
+          return response.json()
+        })
+        .then((data) => {
+          console.log(data)
+          setBlogs(data)
+          setLoading(false)
+        })
+    }, 1000)
   }, [])
 
   return (
     <div className="home">
+      {loading && <Loader />}
       {blogs && <BlogList blogs={blogs} title="All Blogs!" />}
     </div>
   )
